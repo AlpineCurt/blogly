18c493cc521408445cf80276446437ae96edb6c0
Curt <alpinecurt@gmail.com>
2022-09-13 13:25:24 -0700

    create user and user list working

--- app.py
+++ app.py
@@ -16,6 +16,16 @@ debug = DebugToolbarExtension(app)
 connect_db(app)
 #db.create_all()
 
+@app.route("/")
+def home_page():
+    return redirect("/users")
+
+@app.route("/users")
+def user_list():
+    """Shows list of all users"""
+    users = User.query.all()
+    return render_template("user_list.html", users=users)
+
 @app.route("/users/new", methods=["GET"])
 def new_user():
     """Shows form to create a new user"""
@@ -24,4 +34,12 @@ def new_user():
 @app.route("/users/new", methods=["POST"])
 def create_user():
     """Adds user to database"""
-    return "<h1>YOU POSTED!  YAAAAAY!</h1>"
+    first_name = request.form["first_name"]
+    last_name = request.form["last_name"]
+    image_url = request.form["image_url"]
+
+    new_user = User(first_name=first_name, last_name=last_name, image_url=image_url)
+    db.session.add(new_user)
+    db.session.commit()
+
+    return redirect("/users")
